e0f38c9277145b3f596275b0dc20220d3d7db263
TimFB <56914732+TibiIius@users.noreply.github.com>
2023-02-07 22:23:36 +0100

    spelling

--- base/usr/libexec/firstboot/scripts.d/setup-flatpaks.sh
+++ base/usr/libexec/firstboot/scripts.d/setup-flatpaks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-# This script setups Flatpaks on a newly installed system
+# This script sets up Flatpaks on a newly installed system
 
 main() {
   local FLATPAK_FILE=$FILE_PATH/flatpak-done
@@ -11,14 +11,14 @@ main() {
   
   local FLATPAK_EXEC=/usr/bin/flatpak
 
-  # Wait for internet
+  # Wait for internet and DNS
   until /usr/bin/ping -q -c 1 flathub.org; do sleep 1; done
   
-  # Remove Fedora's filtered Flathub repo and add default Flathub repo
+  # Remove Fedora's filtered Flathub repo and add the default one
   $FLATPAK_EXEC remote-delete flathub --force
   $FLATPAK_EXEC remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
   
-  # Replace Fedora Flatpaks with Flathub's versions
+  # Replace Fedora's Flatpaks with Flathub's
   $FLATPAK_EXEC install --noninteractive org.gnome.Platform//43
   $FLATPAK_EXEC install --noninteractive --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
   $FLATPAK_EXEC remote-delete fedora --force
@@ -28,7 +28,7 @@ main() {
   
   # Later on, we will add some more Flatpaks to this list and grab the list automatically from the web
   
-  # Create file to mark that the installation is done
+  # Create file to indicate that the installation is done
   touch $FLATPAK_FILE
 }
 main
